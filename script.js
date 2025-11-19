c091dff52abcdd22c7290da841c4f4f272143ef9
Mohammad Alrassas <xdmohammad83@gmail.com>
2025-11-19 17:11:42 +0300

    finished the logic in the script.js

--- script.js
+++ script.js
@@ -1,64 +1,65 @@
-const playBox = document.querySelectorAll("div");
+// IIFE immediatly invoked function expresion
+const gameBoard = (() => {
+  const _board = new Array(9);
 
-// The board where X and O are placed in
-const Gameboard = ((board) => {
-  this.board = [];
-
-  function play() {
-    mark = GameFlow.getMarker();
-  }
-  const changeBoard = () => {
-    board[cell] = mark;
+  const modifyBoard = (cell, mark) => {
+    _board[cell] = mark;
   };
 
-  return { changeBoard, play };
+  returnBoard = () => {
+    return _board;
+  };
+
+  return { returnBoard, modifyBoard };
 })();
 
-// Players
-function Player(name, marker) {
-  this.name = name;
-  this.marker = marker;
+// Factory for player
+function players(name, marker) {
+  return { name, marker };
 }
 
-// Game Brain !!!
-const GameFlow = ((cell) => {
-  const boy = new Player("Sigma boy", "X");
-  const man = new Player("Aura Man", "Y");
+const gameController = (function () {
+  const xPlayer = players("Player1", "X");
+  const yPlayer = players("Player2", "O");
 
-  let turn;
-  turn = Math.random() <= 0.5 ? "X" : "Y";
+  // random turn between X and O
+  let turn = Math.random() < 0.5 ? "X" : "O";
 
-  for (let i = 0; i < playBox.length; i++) {
-    playBox[i].addEventListener("click", () => {
-      if (turn === "Y" && playBox[i].style.backgroundColor === "") {
-        playBox[i].style.backgroundColor = "green";
-        turn = "X";
-        cell = i;
-        Gameboard.play();
-        console.log(board);
-      } else if (turn === "X" && playBox[i].style.backgroundColor === "") {
-        playBox[i].style.backgroundColor = "blue  ";
-        turn = "Y";
-        cell = i;
-        Gameboard.play();
-        console.log(board);
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  // Add's an event listener to every box
+  const clickBoard = document.querySelectorAll(".play-ground");
+  for (let i = 0; i < clickBoard.length; i++) {
+    clickBoard[i].addEventListener("click", () => {
+      if (clickBoard[i].textContent === "") {
+        clickBoard[i].textContent = turn;
+        gameBoard.modifyBoard(i, turn);
+        turn === "X" ? (turn = "O") : (turn = "X");
+        console.log(gameBoard.returnBoard());
+      }
+
+      // Checks for wins
+      for (const combo of winningCombos) {
+        const [a, b, c] = combo;
+        if (
+          gameBoard.returnBoard()[a] &&
+          gameBoard.returnBoard()[a] === gameBoard.returnBoard()[b] &&
+          gameBoard.returnBoard()[a] === gameBoard.returnBoard()[c]
+        ) {
+          console.log("win");
+        }
       }
     });
   }
-
-  const getTurn = () => {
-    return turn;
-  };
-
-  const getCell = () => {
-    return cell;
-  };
-
-  const getMarker = () => {
-    return Player.marker;
-  };
-
-  return { getCell, getTurn, getMarker };
-
-  console.log(turn);
 })();
